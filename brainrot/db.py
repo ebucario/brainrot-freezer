69503ca55be5c1f70990cad1316fcb4751e5a6ea
ebu <noreply@lucario.dev>
2026-01-20 16:22:57 -0600

    discord working

--- brainrot/db.py
+++ brainrot/db.py
@@ -12,17 +12,17 @@ class Db(UserDict):
 				self.data.update(data)
 		except (FileNotFoundError, json.decoder.JSONDecodeError):
 			print(f"warning: no db file found at \"{DB_PATH}\". recreating db...")
-			self._save()
+			self.save()
 
 	def __setitem__(self, key, item):
 		self.data[key] = item
-		self._save()
+		self.save()
 
 	def __delitem__(self, key):
 		del self.data[key]
-		self._save()
+		self.save()
 	
-	def _save(self):
+	def save(self):
 		with open(DB_PATH, "w") as fp:
 			s = json.dumps(self.data)
 			fp.write(s)
--- brainrot/discord.py
+++ brainrot/discord.py
@@ -1,11 +1,12 @@
-from collections.abc import MutableMapping
 from pathlib import Path
 import discord
 from discord import app_commands
 from discord.ext.commands import Bot
 from threading import Thread
+from typing import cast
 
 from . import sound
+from . import queue
 from .db import db
 
 intents = discord.Intents.default()
@@ -13,41 +14,52 @@ intents.message_content = True
 
 bot = Bot(command_prefix=(), description="""brainrot bot""", intents=intents)
 
-if not isinstance(db.get("channels"), MutableMapping):
-	db["channels"] = {}
+if not isinstance(db.get("channels"), list):
+	db["channels"] = []
 
 @bot.event
 async def on_ready():
 	print(f'logged in as {bot.user}')
 
-@bot.command(description="register this channel for brainrot integration")
-async def brainrot_register_channel(ctx):
-	channel = ctx.channel.id
+async def brainrot_error(inx: discord.Interaction, err: discord.app_commands.AppCommandError):
+	await inx.response.send_message(f"⚠️ error: {err})")
+bot.tree.on_error = brainrot_error
+
+@bot.tree.command(description="register this channel for brainrot integration")
+async def brainrot_register_channel(inx: discord.Interaction):
+	channel = inx.channel.id # type: ignore
 	if channel in db["channels"]:
-		await ctx.send_response("❌ this channel was already registered.", ephemeral=True)
+		await inx.response.send_message("❌ this channel was already registered.", ephemeral=True)
 	else:
 		db["channels"].append(channel)
+		db.save()
 		print(f"registered to channel: {channel}")
-		await ctx.send_response("✅ registered this channel!", ephemeral=True)
+		await inx.response.send_message("✅ registered this channel for brainrot.")
 
-@bot.command(description="remove this channel from brainrot integration")
-async def brainrot_deregister_channel(ctx):
-	channel = ctx.channel.id
+@bot.tree.command(description="remove this channel from brainrot integration")
+async def brainrot_deregister_channel(inx: discord.Interaction):
+	channel = inx.channel.id # type: ignore
 	try:
 		db["channels"].remove(channel)
+		db.save()
 		print(f"removed channel: {channel}")
-		await ctx.send_response("✅ deregistered this channel!", ephemeral=True)
+		await inx.response.send_message("✅ deregistered this channel!")
 	except ValueError:
-		await ctx.send_response("❌ this channel wasn't registered.", ephemeral=True)
+		await inx.response.send_message("❌ this channel wasn't registered.")
 
-@bot.command(description="consume all the messages in a channel")
-async def brainrot_consume(ctx):
-	async for message in reversed(list(ctx.channel.history())):
+@bot.tree.command(description="consume all the messages in a channel")
+async def brainrot_consume(inx: discord.Interaction):
+	if inx.channel.id not in db["channels"]:
+		await inx.response.send_message("❌ this won't work until you register this channel.")
+		return
+	if not hasattr(inx.channel, 'history'):
+		raise ValueError("no history in this channel")
+	async for message in inx.channel.history(oldest_first=True):
 		await on_message(message)
-	await ctx.send_response("✅ successfully consumed the channel!", ephemeral=True)
+	await inx.response.send_message("✅ successfully consumed the channel!")
 
 @bot.event
-async def on_message(message):
+async def on_message(message: discord.Message):
 	if message.author == bot.user:
 		return
 	if message.channel.id not in db["channels"]:
@@ -59,23 +71,23 @@ async def on_message(message):
 			filename = attachment.filename
 			print(f"found sound: {filename}")
 			path = Path(sound.SOUND_PATH, filename)
-			attachment.save(path)
+			await attachment.save(path)
 			print(f"saved to {path}")
-			queue.enqueue(200, sound._load_sounds())
+			queue.enqueue(200, sound._load_sounds)
+			await message.reply("✅ saved \"{path}\"")
 
 if not isinstance(db.get("token"), str):
 	raise TypeError(f"discord token was not string (got {db.get('token')} instead)")
 
-async def setup_hook():
-	print("attempting to sync tree...")
-	print(bot.tree.copy_global_to(guild=discord.Object(id=1012522655460638730)))
-	print(await bot.tree.sync(guild=discord.Object(id=1012522655460638730)))
-	print("tree synced!")
+# async def setup_hook():
+# 	print("attempting to sync tree...")
+# 	print(bot.tree.copy_global_to(guild=discord.Object(id=1012522655460638730)))
+# 	print(await bot.tree.sync(guild=discord.Object(id=1012522655460638730)))
+# 	print("tree synced!")
 
-bot.setup_hook = setup_hook
+# bot.setup_hook = setup_hook
 
 def run_bot():
 	bot.run(db["token"])
 
-# Thread(target=run_bot, daemon=True).start()
-run_bot()
+Thread(target=run_bot, daemon=True).start()
